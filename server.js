cebbb7d40f5199e3163fea4d42494c6fd75ce682
Carlo Rizzante <hello@carlorizzante.com>
2016-12-16 20:14:25 +0100

    Adds Heroku compatibility

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+  if (req.headers["x-forwarded-proto"] === "http") {
+    next();
+  } else {
+    res.redirect("http://" + req.hostname + req.url);
+    // next(); // next here shouldn't be necessary but just in case...
+  }
+});
 
 app.use(express.static("public"));
 
@@ -12,6 +21,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
 
-app.listen(port, () => {
-  console.log(`Express running on port ${port}...`);
+app.listen(PORT, () => {
+  console.log(`Express running on port ${PORT}...`);
 });
